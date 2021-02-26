94daab7289a49f99991cb9b150b355b08d85ec40
AjJ132 <aj132@icloud.com>
2021-02-26 09:07:52 -0500

    asd

---
+++ Excursion/Assets/Scripts/Dynamic Terrain Load and Unload/Territory1On.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Territory1On : MonoBehaviour
+{
+    public GameObject terrain;
+    private string tag;
+    MeshRenderer rend;
+
+
+
+    void Start()
+    {
+        tag = terrain.tag;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            terrain.SetActive(true);
+        }
+    }
+}
