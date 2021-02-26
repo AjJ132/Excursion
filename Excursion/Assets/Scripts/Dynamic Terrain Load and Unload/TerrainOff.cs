38de66f80dfc0fef9bd5e634c0feff15f23c4dc3
AjJ132 <aj132@icloud.com>
2021-02-26 08:43:16 -0500

    asd

---
+++ Excursion/Assets/Scripts/Dynamic Terrain Load and Unload/TerrainOff.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainOff : MonoBehaviour
+{
+    public GameObject terrain;
+    private string tag;
+    MeshRenderer rend;
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        tag = terrain.tag;
+        terrain.SetActive(false);
+    }
+
+}
