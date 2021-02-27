9210354d2974001b6955fccb9e9cddb2088252fb
AjJ132 <aj132@icloud.com>
2021-02-26 19:41:24 -0500

    asd

---
+++ Excursion/Assets/Scripts/Dynamic Terrain Load and Unload/Chunk loading/Chunkdistance.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Chunkdistance : MonoBehaviour
+{
+    public float playerDistance;
+    public Transform player;
+    public GameObject ground;
+    public int chunksApart = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player").transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        playerDistance = Vector3.Distance(player.position, transform.position);
+        if(playerDistance > chunksApart * 175)
+        {
+            ground.SetActive(false);
+        }
+        if(playerDistance <= chunksApart * 175)
+        {
+            ground.SetActive(true);
+        }
+    }
+}
